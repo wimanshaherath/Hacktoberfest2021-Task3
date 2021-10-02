b269d83c6911149cef40a130e1425a9a778aa3b8
wimanshaherath <wimanshah@gmail.com>
2021-10-02 17:34:18 +0530

    Resolve errors in Stack.c

--- Stack.c
+++ Stack.c
@@ -20,9 +20,9 @@ void push(int element)
     }
     else
     {
-        st.top--;
+        st.top++;
         printf("\nEnter a Value ");
-        scanf("%s",&ele);
+        scanf("%d",&ele);
         st.arr[st.top]=ele;
     }
 }
@@ -47,7 +47,7 @@ int pop()
 int peek()
 {
     int display;
-    display=st.arr[st.rear];
+    display=st.arr[st.top];
     return display;
 }
 
@@ -57,7 +57,7 @@ void display()
     if((st.top)>=0)
     {
         printf("\n\nElements in the Stack");
-        for(i=st.top;i>=0;i++)
+        for(int i=st.top;i>=0;i--)
         {
             printf("\n%d",st.arr[i]);
         }
@@ -78,7 +78,7 @@ int main()
 
     do{
         printf("\nEnter Your Choice  ");
-        scanf("%c",&choice);
+        scanf("%d",&choice);
         switch(choice)
         {
         case 1:
@@ -87,11 +87,11 @@ int main()
             }
         case 2:
             {
-                printf("%d",pop());
+                printf("%d",pop());break;
             }
         case 3:
             {
-                printf("%d",peek());
+                printf("%d",peek());break;
             }
         case 4:
             {
